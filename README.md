405fe402da8fc2fd20d3bd52de701a7809717983
Carter Medlin <carterm@users.noreply.github.com>
2021-09-09 15:20:15 -0700

    First push

--- README.md
+++ README.md
@@ -1,2 +1,106 @@
-# 11ty-serverless-preview-mode
-Serverless rendering of a single 11ty page using Wordpress API data
+# eleventy serverless Preview Mode for Azure FaaS
+
+## Features
+- Single page 11ty rendering of content retrieved from your data source (Wordpress API, GitHub?)
+
+## Intent
+* Using an existing 11ty project, add the ability to render a single page from Wordpress with an Azure function.
+* Provide access to unpublished "preview" data
+* Make a distributable module to do this
+
+## Not obvious concepts
+* resource links (.css, .png, etc) directed back at the service will redirect to the main site
+
+## Current situtation
+* Can render a single page from Wordpress
+* Module still needs to be refined to be more distributable
+* Update the sample below to be more generic
+
+
+## Sample preview mode page template ##
+Add this to your 11ty `pages` folder as `previewModePage.11ty.js` to support dynamic rendering.  
+```
+const { addPreviewModeDataElements, getPostJsonFromWordpress } = require("@cagov/11ty-serverless-preview-mode");
+
+const wordPressSettings = {
+    wordPressSite: "https://live-odi-content-api.pantheonsite.io", //Wordpress endpoint
+    previewWordPressTagId: 20 //preview-mode tag id in Wordpress
+}
+
+class previewModePageClass {
+    /**
+     * First, mostly static.  Returns the frontmatter data.
+     */
+    async data() {
+        return {
+            layout: "page", //Or whatever layout the preview page should have
+            tags: ["news"], //Or whatever tags the preview page should have
+            ...addPreviewModeDataElements()
+        };
+    }
+
+    /**
+     * Last, after the frontmatter data is loaded.  Able to render the page.
+     * @param {{ title: string; publishdate: string; meta: string; description: string; lead: string; author: string; previewimage: string; eleventy: { serverless: { query: { postid?: string; }; }; }; }} itemData
+     */
+    async render(itemData) {
+        const jsonData = await getPostJsonFromWordpress(itemData, wordPressSettings);
+
+        let featuredMedia = jsonData._embedded["wp:featuredmedia"];
+
+        //Customize for you templates
+        itemData.title = jsonData.title.rendered;
+        itemData.publishdate = jsonData.date.split('T')[0]; //new Date(jsonData.modified_gmt)
+        itemData.meta = jsonData.excerpt.rendered;
+        itemData.description = jsonData.excerpt.rendered;
+        itemData.lead = jsonData.excerpt.rendered;
+        itemData.author = jsonData._embedded.author[0].name;
+        itemData.previewimage = featuredMedia ? featuredMedia[0].source_url : "img/thumb/APIs-Blog-Postman-Screenshot-1.jpg";
+
+        return jsonData.content.rendered;
+    }
+}
+
+module.exports = previewModePageClass;
+```
+
+## Add this to `.eleventy.js` ##
+```
+  const { addPreviewModeToEleventy } = require("@cagov/11ty-serverless-preview-mode");
+  addPreviewModeToEleventy(eleventyConfig);
+```
+
+## For Azure Faas ##
+`index.js`
+```
+const { serverlessHandler } = require("@cagov/11ty-serverless-preview-mode");
+const contentRedirectSiteTarget = "https://digital.ca.gov";
+
+/**
+ * Azure Function to render a single 11ty page
+ * @param {{res:{statusCode:number;body:string;headers?:*};done:function}} context
+ * @param {{params:{segments?:*},headers:*,query:*}} req
+ */
+module.exports = async function (context, req) {
+  try {
+    if (req.params.segments) { // Resource call, redirect back to the main site
+      context.res = { statusCode: 301, headers: { location: `${contentRedirectSiteTarget}${req.headers["x-original-url"]}` }, body: null };
+    } else {
+      context.res = await serverlessHandler(req.query);
+    }
+
+  } catch (error) {
+    context.res = {
+      status: error.httpStatusCode || 500,
+      body: JSON.stringify(
+        {
+          error: error.message,
+        },
+        null,
+        2
+      ),
+    };
+  }
+  if (context.done) context.done();
+}
+```
---
+++ index.js
@@ -0,0 +1,145 @@
+
+//@ts-check
+const fetch = require('node-fetch/lib');
+const serverlessFunctionName = "possum";
+const pagePath = "/GeneratePreviewModePath";
+const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
+/** @type WordpressPostRow */
+const digestPageJSON = require('./digestPageJson.json');
+
+/**
+ * Adds EleventyServerless with simple config for single page rendering
+ * @param {*} eleventyConfig 
+ * @example
+ * module.exports = function(eleventyConfig) {
+ *   const path = require('path'); //Path Resolve needed to make plugin mode copy work
+ *   const { addPreviewModeToEleventy } = require( path.resolve('.','./previewModeModule/addPreviewModeToEleventy') );
+ *   addPreviewModeToEleventy(eleventyConfig);
+ * }
+ */
+const addPreviewModeToEleventy = eleventyConfig => {
+    eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+        name: serverlessFunctionName, // The serverless function name from your permalink object
+        inputDir: "",
+        functionsDir: "", //off the root
+        redirects: "", //no redirect handling built in
+        copyOptions: {
+            filter: ['**/*', '!**']
+        } // Filtering out all pages, this still brings in includes
+    });
+};
+
+/**
+ * runs serverless eleventy on the default page.  Returns a function response.
+ * @param {*} queryStringParameters from your function's request `req.query`
+ * @returns {Promise<{statusCode:number, headers:{"Content-Type":string},body:string}>} Function response Promise
+ * @example context.res = await serverlessHandler(req.query); //Azure FaaS
+ */
+const serverlessHandler = async queryStringParameters => {
+    const path = require('path'); //Path Resolve needed to make plugin mode copy work
+    const xpath = path.resolve(".", serverlessFunctionName);
+    const serverlessFolder = require(xpath);
+    return serverlessFolder.handler({ path: pagePath, queryStringParameters });
+};
+
+/**
+* @typedef {Object} WordpressPostRow Expected POST input when using the Wordpress API - https://developer.wordpress.org/rest-api/reference/posts/
+* @property {number} author
+* @property {number[]} categories
+* @property {string} comment_status "closed"
+* @property {{rendered:string}} content
+* @property {string} date
+* @property {string} date_gmt
+* @property {{rendered:string}} excerpt
+* @property {number} featured_media
+* @property {string} format
+* @property {{rendered:string}} guid
+* @property {number} id
+* @property {string} link
+* @property {any[]} meta
+* @property {string} modified
+* @property {string} modified_gmt
+* @property {string} ping_status "closed"
+* @property {string} slug
+* @property {string} status "publish"
+* @property {boolean} sticky
+* @property {number[]} tags
+* @property {string} template
+* @property {{rendered:string}} title
+* @property {string} type "post"
+* @property {{"wp:featuredmedia"?:{source_url:string}[],author:{name:string}[]}} [_embedded]
+* @property {*} [_links]
+*/
+
+/**
+* @typedef {Object} WordpressSettings
+* @property {string} wordPressSite
+* @property {number} previewWordPressTagId
+*/
+
+/**
+ * calls fetch and expects a json result.  Error on non-ok status.
+ * @param {string} url 
+ * @param {*} [opts]
+ */
+const fetchJson = async (url, opts) => {
+    const fetchResponse = await fetch(url, opts);
+    if (!fetchResponse.ok) {
+        throw new Error(`${fetchResponse.status} - ${fetchResponse.statusText} - ${fetchResponse.url}`);
+    }
+    return fetchResponse.json();
+}
+
+/**
+ * @param {{ eleventy: { serverless: { query: { postid?: string}}}}} itemData
+ * @param {WordpressSettings} wordpressSettings
+ * @returns {Promise<WordpressPostRow>}
+ * @example
+ * async render(itemData) {
+ *   const jsonData = await getPostJsonFromWordpress(itemData,wordPressSettings);
+ *   return jsonData.content.rendered;
+ * }
+ */
+const getPostJsonFromWordpress = async (itemData, wordpressSettings) => {
+    if (itemData.eleventy.serverless.query.postid) {
+        const wpApiPage = `${wordpressSettings.wordPressSite}/wp-json/wp/v2/posts/${itemData.eleventy.serverless.query.postid}?_embed&cachebust=${Math.random()}`;
+
+        return fetchJson(wpApiPage);
+    } else {
+        const wpApiPage = `${wordpressSettings.wordPressSite}/wp-json/wp/v2/posts/?tags=${wordpressSettings.previewWordPressTagId}&orderby=modified&_fields=title,modified,id&cachebust=${Math.random()}`;
+
+        return fetchJson(wpApiPage)
+            .then((/** @type {{id:number,title:{rendered:string},modified:string}[]} */ previewPosts) => {
+                const links = previewPosts.map(x => `<li><a href="?postid=${x.id}">${x.title.rendered}</a> - ${x.modified}</li>`);
+
+                let digestReturn = { ...digestPageJSON };
+                digestReturn.content.rendered = `<ul>${links.join()}</ul>`;
+                return digestReturn
+            });
+    }
+}
+
+/**
+ * Puts the correct permalink in the data section
+ * @example async data() {
+ *        return {
+ *           layout: "page",
+ *           tags: ["news"],
+ *           ...addPreviewModeDataElements()
+ *       };
+ *   }
+ */
+ const addPreviewModeDataElements = () => (
+    {
+        permalink: {
+            [serverlessFunctionName]: pagePath
+        }
+    }
+);
+
+module.exports = {
+    serverlessHandler,
+    getPostJsonFromWordpress,
+    addPreviewModeToEleventy,
+    addPreviewModeDataElements
+}
